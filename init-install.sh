0f20a2b5803c6994c641cf8e1bb436631c7dff69
Seonyoung-Cheon <sunyoung7708@yonsei.ac.kr>
2024-09-20 13:38:42 +0000

    Fix shell file

--- init-install.sh
+++ init-install.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-### vim setting
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/morhetz/gruvbox.git ~/.vim/plugged/gruvbox
-vim -c 'PlugInstall' -c 'qa!'
-
-### tmux conf setting
-# Install tmux tpm if it is not installed
-if [[ ! -d ~/.tmux/plugins/tpm ]]; then
-  echo "Installing tmux tpm..."
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-tmux source-file ~/.tmux.conf
+### install zsh
+sudo apt-get install -y zsh
+sudo apt install -y curl
 
 ### zsh setting
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp ~/.init-setting/.zshrc ~/.zshrc
 chsh -s /bin/zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+### oh my zsh plugin
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
@@ -26,8 +18,23 @@ cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/autojump
 python3 ./install.py
 cd ~
 
-exec zsh
 source $ZSH/oh-my-zsh.sh
 source ~/.zshrc
-p10k configure
+
+### powerlevel 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp ~/.init-setting/.p10k.zsh ~/.p10k.zsh
+#p10k configure
+
+### tmux conf setting
+sudo apt-get install -y tmux
+
+# Install tmux tpm if it is not installed
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
+  echo "Installing tmux tpm..."
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+cp ~/.init-setting/.tmux.conf ~/.tmux.conf
+tmux source-file ~/.tmux.conf
 
--- setting.sh
+++ setting.sh
@@ -5,21 +5,13 @@ cp ~/.init-setting/.vimrc ~/.vimrc
 cp ~/.init-setting/.vim/syntax ~/.vim/ -r
 
 ### vim setting
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/plugged/gruvbox
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 vim -c 'PlugInstall' -c 'qa!'
 export TERM=xterm-256color
 cd ~/.vim/plugged/YouCompleteMe
 python3 install.py --clangd-completer
 cd ~
 
-
 export TERM=xterm-256color
-
-### tmux conf setting
-cp ~/.init-setting/.tmux.conf ~/.tmux.conf
-tmux source-file ~/.tmux.conf
-
-### zsh setting
-cp ~/.init-setting/.zshrc ~/.zshrc
-cp ~/.init-setting/.p10k.zsh ~/.p10k.zsh
-# p10k configure
-
