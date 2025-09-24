bb8be3c5041b3bfd2b723866a06a22d39f342400
seonyoung <seonyoung@yonsei.ac.kr>
2025-09-24 22:02:11 +0900

    Modify update script

--- update-plugins.sh
+++ update-plugins.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-cp -r ./nvim ~/.config/nvim
+cp -r ./nvim/lua/plugins ~/.config/nvim/lua
 
 echo "Running :Lazy sync to install plugins..."
 $(pwd)/nvim-linux-x86_64/bin/nvim --headless -c "Lazy! sync" -c "qa" 
