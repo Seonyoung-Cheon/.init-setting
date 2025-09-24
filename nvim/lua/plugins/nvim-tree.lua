4022e774cc93ab19f853895a452ae05e75e61b70
seonyoung <seonyoung@yonsei.ac.kr>
2025-09-24 21:49:42 +0900

    NerdTree and Tab Upgrade

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,16 +1,33 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup({
-      git = {
-        enable = true,
-        ignore = false,
-      }
-    })
-  end,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				local opts = { buffer = bufnr, noremap = true, silent = true, nowait = true }
+
+				api.config.mappings.default_on_attach(bufnr)
+
+				vim.keymap.set("n", "v", api.node.open.vertical, opts)
+				vim.keymap.set("n", "s", api.node.open.horizontal, opts)
+				vim.keymap.set("n", "i", api.node.open.tab, opts)
+				vim.keymap.set("n", "C", api.tree.change_root_to_node, opts)
+				vim.keymap.set("n", "u", api.tree.change_root_to_parent, opts)
+			end,
+			git = {
+				enable = true,
+				ignore = false,
+			}
+		})
+	end,
+	keys = {
+		{ "<F4>", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
+	}
+
 }
